14364c0aa3fb7e1941ec074325dc402bf8da5818
Jamal Theodore <thesocial.congos@xoxy.net>
2017-12-20 09:09:53 -0500

    GO app keep alive

---
+++ keep-alive-GO-vpn.ps1
@@ -0,0 +1,7 @@
+﻿do { 
+$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate('User Desktop - Desktop Viewer')
+Sleep 1
+$wshell.SendKeys('#')
+    start-sleep -Seconds 900
+}until($infinity)
