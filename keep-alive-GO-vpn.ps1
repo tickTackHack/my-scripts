e8be173b58b530639c23bfd4c43bd025d1524a76
Jamal <github.congos@xoxy.net>
2017-12-20 23:31:54 -0500

    Delete keep-alive-GO-vpn.ps1

--- keep-alive-GO-vpn.ps1
+++
@@ -1,7 +0,0 @@
-﻿do { 
-$wshell = New-Object -ComObject wscript.shell;
-$wshell.AppActivate('User Desktop - Desktop Viewer')
-Sleep 1
-$wshell.SendKeys('#')
-    start-sleep -Seconds 900
-}until($infinity)
